c45f9390eb3e688f12800cfb17d2e277e41dc2b0
ErfanK96 <erfankabiri1375@yahoo.com>
2025-11-02 23:43:05 +0330

    utc time
    utc time optin added to command

--- src/time_commands.rs
+++ src/time_commands.rs
@@ -1,10 +1,16 @@
-
 // Handle commands
-match input {
-    "hello" => println!("Hello there!"),
-    "time" => {
-        let now = chrono::Local::now();
-        println!("Current time: {}", now);
+pub fn timeCommand(args: &[&str]) {
+
+    match args {
+        ["now"] => {
+            let now = chrono::Local::now();
+            println!("Local time: {}", now.format("%Y-%m-%d %H:%M:%S"));
         }
-    _ => println!("Unknown command: {}", input),
+        ["utc"] => {
+            let now = chrono::Utc::now();
+            println!("UTC time: {}", now.format("%Y-%m-%d %H:%M:%S"));
+        }
+        _ => println!("Usage: time <now|utc>"),
+    }
+
 }
