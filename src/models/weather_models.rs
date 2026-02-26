a104127d4fbc7608ebf6ffa50bd7ea588732901e
ErfanK96 <erfankabiri1375@yahoo.com>
2026-02-26 03:53:49 +0330

    delete miscelenous fields from model
    feels_like and humidity are not used in new api.

--- src/models/weather_models.rs
+++ src/models/weather_models.rs
@@ -3,8 +3,6 @@ pub struct WeatherResponse {
     pub name: String,
     pub country: String,
     pub temp: f64,
-    pub feels_like: f64,
     pub description: String,
-    pub humidity: u8,
     pub wind_speed: f64,
 }
